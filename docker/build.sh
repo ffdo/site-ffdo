4fe928e8a5448f51890acb670bc192ca76957501
Markus Lindenberg <lindenberg@gonicus.de>
2015-11-11 23:38:53 +0100

    Fix output path

--- docker/build.sh
+++ docker/build.sh
@@ -20,4 +20,4 @@ time make -j $(($(nproc)+1)) V=s BROKEN=1 GLUON_TARGET=ar71xx-generic
 set +x
 echo -e "\nBUILD FINISHED\n"
 echo "You can copy the resulting images from the container using:"
-echo -e "\ndocker cp ${HOSTNAME}:/usr/src/build/gluon/images <destination>\n"
+echo -e "\ndocker cp ${HOSTNAME}:/usr/src/build/gluon/output <destination>\n"
